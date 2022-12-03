7bce1c9e9b46b8e56bac58696970f570c276d068
Ankhena <Ankhena@users.noreply.github.com>
2022-12-03 23:47:58 +0000

    Deploying to gh-pages from @ Ankhena/0066_SocialLead@387d429ec2ce8b2753a22eaf95565709b2856ca0 🚀

--- js/script.js
+++ js/script.js
@@ -245,17 +245,17 @@ const tgSubmitBtn = document.querySelector('#tg-form [type="submit"]');
 
 let tgFormUser;
 let tgFormCommunication;
-let tgFormText;
+let tgFormPhone;
 let tgFormConditions;
 let tgFormAction;
+let tgFormBrand;
 
 // клиент
-// let tgChatID = '-619933979';
-// let tgChatBot = '2079108145:AAG22VqgLvORI8_wo8uVcOwHgIVo28hVH6Q'
+// let tgChatID = '-5855694826';
+// let tgChatBot = '5855694826:AAFV3eIS5_46SDYxnp4U7bYEaGdhgErXp0k'
 
 let tgChatID = '-1001775101104';
 let tgChatBot = '1957445015:AAEI6SmBmXdg3V2e-IBy1cCEbg0OkKJreFQ'
-
 
 tgFields.forEach((field) =>
   field.addEventListener("input", (e) => {
@@ -265,13 +265,16 @@ tgFields.forEach((field) =>
       tgFormUser = e.target.value;
     } else if (fieldName === "email") {
       tgFormCommunication = e.target.value;
+    }else if (fieldName === "brand") {
+      tgFormBrand = e.target.value;
     } else if (fieldName === "phone") {
-      tgFormText = e.target.value;
+      tgFormPhone = e.target.value;
     }
     else if (fieldName === "conditions") {
       tgFormConditions = e.target.value;
     }
-    tgFormAction = `https://api.telegram.org/bot${tgChatBot}/sendMessage?chat_id=${tgChatID}&parse_mode=html&&text=%3Cb%3EName%3C/b%3E%0A${tgFormUser}%0A%3Cb%3EPreferred%20communication%20method%3C/b%3E%0A${tgFormCommunication}%0A%3Cb%3EMessage%3C/b%3E%0A${tgFormText}`;
+    //tgFormAction = `https://api.telegram.org/bot${tgChatBot}/sendMessage?chat_id=${tgChatID}&parse_mode=html&&text=%3Cb%3EName%3C/b%3E%0A${tgFormUser}%0A%3Cb%3EPreferred%20communication%20method%3C/b%3E%0A${tgFormCommunication}%0A%3Cb%3EMessage%3C/b%3E%0A${tgFormText}`;
+    tgFormAction = `https://api.telegram.org/bot${tgChatBot}/sendMessage?chat_id=${tgChatID}&parse_mode=html&&text=%3Cb%3EИмя%3C/b%3E%0A${tgFormUser}%0A%3Cb%3EБренд%3C/b%3E%0A${tgFormBrand}%0A%3Cb%3EEmail%3C/b%3E%0A${tgFormCommunication}%3Cb%3E%0AТелефон%3C/b%3E%0A${tgFormPhone} %3Cb%3E%0AСогласен%20с%20правилами%3C/b%3E%0A${tgFormConditions}`;
 
     tgForm.setAttribute("action", tgFormAction);
   })
@@ -286,7 +289,7 @@ tgSubmitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   // tgForm.submit();
 
-  if (tgFormUser && tgFormCommunication && tgFormText) {
+  if (tgFormUser && tgFormCommunication && tgFormPhone) {
     fetch(tgFormAction, { method: "POST" });
     okBlock.classList.remove("hide");
   } else {
